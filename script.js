b05808fba9785a6eaab65338a3b869dbd691ab28
rdnsa <rdnsa123@gmail.com>
2024-12-19 11:32:24 +0700

    initial pertama aku

---
+++ script.js
@@ -0,0 +1,8 @@
+// Memuat animasi Lottie
+lottie.loadAnimation({
+    container: document.getElementById('lottie-container'), // Tempat animasi ditampilkan
+    renderer: 'svg', // Format render
+    loop: true,      // Animasi berulang
+    autoplay: true,  // Animasi dimulai otomatis
+    path: 'https://lottie.host/99c8597f-dd05-4627-bcd7-e385377cd7b0/8RDi943nUV.json' // URL animasi Lottie
+});
